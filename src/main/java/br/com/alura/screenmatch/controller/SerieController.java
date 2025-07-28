2ab8da20c54c3f4497feece000106d23d7249a59
Gabriel Valadares <silva44gabriel@gmail.com>
2025-07-28 10:55:23 -0300

    feat: adiciona busca por id

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,6 +4,7 @@ import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +26,14 @@ public class SerieController {
     public List<SerieDTO> obterTop5Series() {
         return servico.obterTop5Series();
     }
+
     @GetMapping("/lancamentos")
     public List<SerieDTO> obterLancamentos(){
         return servico.obterLancamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obteerPorId(@PathVariable Long id) {
+        return servico.opterPorId(id);
     }
 }
